608f12c0e33bac7ce676f1e121201decb477424b
Jinyang Yu <jy2803@columbia.edu>
2017-04-06 14:02:43 -0400

    F

---
+++ test.js
@@ -0,0 +1,48 @@
+/**
+ * Created by jinyangyu on 4/6/17.
+ */
+var assert = require('assert');
+var expect = require("chai").expect;
+var unitfunc = require("./unitfunctions");
+var config = require('./config.js');
+var funct = require("./functions");
+var mongodbUrl = 'mongodb://' + config.mongodbHost + ':27017/test';
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectID;
+
+describe('Books', function () {
+    describe('#PublishNewBook', function () {
+        it("publishes a new book", function () {
+            var book = unitfunc.publishBook('a','b','c','d','e');
+            expect(book.bookname).to.equal('a');
+        })
+    });
+    describe('#PublishNewChapter', function () {
+        it("publishes a new chapter", function () {
+            var chapter = unitfunc.publishChapter('a', 'b', 'c', 'd');
+            expect(chapter.chpname).to.equal('a');
+        })
+    });
+
+});
+describe('Users', function () {
+    describe('#Sign up', function () {
+        it("signs up a new user", function () {
+            funct.localReg('testwriter','testwriter','writer');
+            MongoClient.connect(mongodbUrl).then(function (db) {
+                var users = db.collection('Users');
+                users.findOne({
+                    'username': 'testwriter'
+                }).then(function (user) {
+                    expect(user.username).to.equal('testwriter');
+                })
+            })
+        })
+    });
+    describe('#Sign in', function () {
+        it("signs in an old user", function () {
+
+        })
+    })
+});
+
